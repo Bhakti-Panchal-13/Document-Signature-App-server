26dbd9cf3d123be189c5c024fffd33c09bef4351
Bhakti-Panchal-13 <bhaktipanchal13g@gmail.com>
2025-07-06 00:22:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,8 @@ app.use(session({
   saveUninitialized: false,
   cookie: {
     httpOnly: true,
-    secure: false, // set true if using HTTPS
+    secure: true, // set true if using HTTPS
+    sameSite: 'none',
     maxAge: 1000 * 60 * 60 * 24, // 1 day
   },
 }));
